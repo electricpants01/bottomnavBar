d3c60f7c6262b77806c51f2eaa527f4d91051c19
Christian Torrico <christorricoavila@gmail.com>
2021-05-13 15:19:52 -0400

    source code

---
+++ app/src/main/java/com/christian/bottomnavigation/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.christian.bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.christian.bottomnavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navBar = binding.mainNavBar
+        val navController = findNavController(R.id.fragment)
+        navBar.setupWithNavController(navController)
+    }
+}
